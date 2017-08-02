06843b39a4ea06aab01b2f525bb159e9cceb2e14
strider90arun <strider90arun@gmail.com>
2017-08-02 15:22:25 -0700

    Adding lombok dependency

--- src/main/java/com/arun/config/AppConfig.java
+++ src/main/java/com/arun/config/AppConfig.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@ComponentScan(basePackages = "com.bootmongo")
+@ComponentScan(basePackages = "com.arun")
 @EnableMongoRepositories
 @EnableAutoConfiguration
 @SpringBootApplication
