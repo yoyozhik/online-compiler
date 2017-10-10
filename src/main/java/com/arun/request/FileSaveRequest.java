8aef1c9f05f8c459f4e24538f561835da9c8911a
strider90arun <strider90arun@gmail.com>
2017-10-10 10:45:06 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/request/FileSaveRequest.java
+++ src/main/java/com/arun/request/FileSaveRequest.java
@@ -1,6 +1,7 @@
 package com.arun.request;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import java.io.File;
 import java.util.List;
@@ -8,14 +9,31 @@ import java.util.List;
 @Data
 public class FileSaveRequest {
 
+	@ApiObjectField(description = "Question Id")
 	private String questionId;
+
+	@ApiObjectField(description = "Code")
 	private File code;
+
+	@ApiObjectField(description = "Description")
 	private String problemDescription;
+
+	@ApiObjectField(description = "Title")
 	private String problemTitle;
+
+	@ApiObjectField(description = "Language")
 	private String language;
+
+	@ApiObjectField(description = "Admin Id")
 	private Integer adminId;
+
+	@ApiObjectField(description = "User Name")
 	private String adminUserName;
+
+	@ApiObjectField(description = "Participants")
 	private List<String> participants;
+
+	@ApiObjectField(description = "Answered By")
 	private String answeredBy;
 
 }
