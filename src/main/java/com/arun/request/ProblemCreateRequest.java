bb71237e15aca5346797be7a6528c36d33a92826
strider90arun <strider90arun@gmail.com>
2017-10-11 13:46:02 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/request/ProblemCreateRequest.java
+++ src/main/java/com/arun/request/ProblemCreateRequest.java
@@ -1,21 +1,41 @@
 package com.arun.request;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import java.util.List;
 
 @Data
 public class ProblemCreateRequest {
 
+	@ApiObjectField(description = "Code")
 	private List<String> code;
+
+	@ApiObjectField(description = "Description")
 	private String problemDescription;
+
+	@ApiObjectField(description = "Problem Title")
 	private String problemTitle;
+
+	@ApiObjectField(description = "Language")
 	private String language;
+
+	@ApiObjectField(description = "Question Id")
 	private String questionId;
+
+	@ApiObjectField(description = "Class Name")
 	private String className;
+
+	@ApiObjectField(description = "Admin Id")
 	private Integer adminId;
+
+	@ApiObjectField(description = "Admin User Name")
 	private String adminUserName;
+
+	@ApiObjectField(description = "Participants")
 	private List<String> participants;
+
+	@ApiObjectField(description = "Answered By")
 	private String answeredBy;
 
 }
