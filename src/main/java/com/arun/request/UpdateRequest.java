ff393a10bcfbfd99645dabac908d8da15a86a395
strider90arun <strider90arun@gmail.com>
2017-10-11 13:46:42 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/request/UpdateRequest.java
+++ src/main/java/com/arun/request/UpdateRequest.java
@@ -1,12 +1,18 @@
 package com.arun.request;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 @Data
 public class UpdateRequest {
 
+	@ApiObjectField(description = "Question Id")
 	private String questionId;
+
+	@ApiObjectField(description = "Feedback")
 	private String feedback;
+
+	@ApiObjectField(description = "Rating")
 	private String rating;
 
 }
