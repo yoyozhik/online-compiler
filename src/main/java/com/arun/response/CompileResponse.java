599d7b380b71ce39e7f09ec042c1df15058cb7c5
strider90arun <strider90arun@gmail.com>
2017-10-12 10:30:27 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/response/CompileResponse.java
+++ src/main/java/com/arun/response/CompileResponse.java
@@ -1,13 +1,17 @@
 package com.arun.response;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import java.util.List;
 
 @Data
 public class CompileResponse {
 
+	@ApiObjectField(description = "Compiled Output")
 	private List<String> compiledOutput;
+
+	@ApiObjectField(description = "Flag")
 	private boolean flag;
 
 }
