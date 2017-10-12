12249efeaacdd0d72ed3bd994937edd8cc8ede4a
strider90arun <strider90arun@gmail.com>
2017-10-12 10:29:48 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/response/CodeDetailsWrapper.java
+++ src/main/java/com/arun/response/CodeDetailsWrapper.java
@@ -1,13 +1,17 @@
 package com.arun.response;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import java.util.List;
 
 @Data
 public class CodeDetailsWrapper {
 
+	@ApiObjectField(description = "Problem")
 	private ProblemCode problem;
+
+	@ApiObjectField(description = "List of Answers")
 	private List<ProblemCode> answers;
 
 }
