4da9b15a5af5ec506bb94d973ff7881a0cf317a6
strider90arun <strider90arun@gmail.com>
2017-10-13 09:43:51 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/response/ProblemResponse.java
+++ src/main/java/com/arun/response/ProblemResponse.java
@@ -2,11 +2,15 @@ package com.arun.response;
 
 import com.arun.domain.MetaData;
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 @Data
 public class ProblemResponse {
 
+	@ApiObjectField(description = "Problem Id")
 	private String problemId;
+
+	@ApiObjectField(description = "Metadata")
 	private MetaData metaData;
 
 }
