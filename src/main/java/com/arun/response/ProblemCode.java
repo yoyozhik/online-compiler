ee19cd6e4149e453c5c3ea737c695e480f2f24d0
strider90arun <strider90arun@gmail.com>
2017-10-13 09:40:10 -0700

    Json Doc Api Query params

--- src/main/java/com/arun/response/ProblemCode.java
+++ src/main/java/com/arun/response/ProblemCode.java
@@ -1,19 +1,35 @@
 package com.arun.response;
 
 import lombok.Data;
+import org.jsondoc.core.annotation.ApiObjectField;
 
 import java.util.List;
 
 @Data
 public class ProblemCode {
 
+	@ApiObjectField(description = "Problem Id")
 	private String problemId;
+
+	@ApiObjectField(description = "Problem Description")
 	private String problemDesc;
+
+	@ApiObjectField(description = "Problem Title")
 	private String problemTitle;
+
+	@ApiObjectField(description = "Code Lines")
 	private List<String> codeLines;
+
+	@ApiObjectField(description = "Answered By")
 	private String answeredBy;
+
+	@ApiObjectField(description = "Class Name")
 	private String className;
+
+	@ApiObjectField(description = "Feedback")
 	private String feedback;
+
+	@ApiObjectField(description = "Rating")
 	private String rating;
 
 }
