75c61c343919cd1451f9a053a323b8a240ecd4f7
strider90arun <strider90arun@gmail.com>
2017-10-15 17:00:00 -0700

    Comments for File Operations

--- src/main/java/com/arun/helper/FileOperations.java
+++ src/main/java/com/arun/helper/FileOperations.java
@@ -6,12 +6,38 @@ import java.util.List;
 
 public interface FileOperations {
 
+	/**
+	 * Create a Java source file from problem code
+	 *
+	 * @param codeLines
+	 * @param className
+	 * @param language
+	 * @return
+	 */
 	File createFile(List<String> codeLines, String className, String language);
-		
+
+	/**
+	 * Compile and check for errors
+	 *
+	 * @param file
+	 * @return
+	 */
 	List<String> syntaxChecker(String file);
-	
+
+	/**
+	 * Execute Java code file
+	 *
+	 * @param file
+	 * @return
+	 */
 	List<String> executeCode(File file);
-	
+
+	/**
+	 * Execute python source file
+	 *
+	 * @param file
+	 * @return
+	 */
 	List<String> executePythonCode(File file);
 	
 }
